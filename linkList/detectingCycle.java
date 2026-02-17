c8254044a662da256794068a7240968afbfe5a6e
Deepak Singh Soun <deepaksoun12@gmail.com>
2026-02-18 02:59:18 +0530

    add new code

---
+++ linkList/detectingCycle.java
@@ -0,0 +1,46 @@
+package linkList;
+
+public class detectiongCycle {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+
+    public boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        detectiongCycle list = new detectiongCycle();
+
+        list.head = list.new Node(1);
+        list.head.next = list.new Node(2);
+        list.head.next.next = list.new Node(3);
+        list.head.next.next.next = list.new Node(4);
+
+        // Create cycle
+        list.head.next.next.next.next = list.head;
+
+        System.out.println(list.isCycle());
+    }
+}
