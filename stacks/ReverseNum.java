579a1c8316282c5f400a2a4d7f738328b0868557
Deepak Singh Soun <deepaksoun12@gmail.com>
2026-03-10 04:14:14 +0530

    new code

--- stacks/ReverseNum.java
+++ stacks/ReverseNum.java
@@ -2,9 +2,9 @@ package stacks;
 
 import java.util.*;
 
-public class PushAtBottom {
+public class ReverseNum {
 
-    public static void AtBottom(Stack<Integer> s, int data) {
+    public static void pushAtBottom(Stack<Integer> s, int data) {
 
         if (s.isEmpty()) {
             s.push(data);
@@ -12,8 +12,19 @@ public class PushAtBottom {
         }
 
         int top = s.pop();
-        AtBottom(s, data);
+        pushAtBottom(s, data);
         s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
     }
 
     public static void printStacks(Stack<Integer> s) {
@@ -25,12 +36,11 @@ public class PushAtBottom {
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
 
-        s.push(3);
-        s.push(2);
         s.push(1);
+        s.push(2);
+        s.push(3);
 
-        AtBottom(s, 4);
-
+        reverse(s);
         printStacks(s);
     }
 }
