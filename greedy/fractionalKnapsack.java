5bb511c92bbcf24d1da9649b0632602db0695cd3
Deepak Singh Soun <deepaksoun12@gmail.com>
2026-03-30 02:05:48 +0530

    add new code knapsack in greedy flouder

---
+++ greedy/fractionalKnapsack.java
@@ -0,0 +1,36 @@
+package greedy;
+
+import java.util.*;
+
+public class fractionalKnapsack {
+    public static void main(String[] args) {
+        int[] val = { 60, 100, 120 };
+        int[] weight = { 10, 20, 30 };
+        int w = 50;
+
+        double[][] ratio = new double[val.length][2];
+
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = val[i] / (double) weight[i];
+        }
+
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+        double finalVal = 0;
+        int capacity = w;
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int) ratio[i][0];
+            if (capacity >= weight[idx]) {
+                finalVal += val[idx];
+                capacity -= weight[idx];
+            } else {
+                finalVal += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
+
+        System.out.println("Final Value : " + finalVal);
+    }
+}
