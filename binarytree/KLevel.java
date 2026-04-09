f53cdc33d4c4bebc175bf5235009da90b056a3d8
Deepak Singh Soun <deepaksoun12@gmail.com>
2026-04-09 20:41:24 +0530

    new code

---
+++ binarytree/KLevel.java
@@ -0,0 +1,41 @@
+package binarytree;
+
+public class KLevel {
+    static class Node {
+        int val;
+        Node left, right;
+
+        Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void kthLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+
+        if (level == k) {
+            System.out.print(root.val + " ");
+            return;
+        }
+
+        kthLevel(root.left, level + 1, k);
+        kthLevel(root.right, level + 1, k);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 2;
+        kthLevel(root, 1, k);
+    }
+}
